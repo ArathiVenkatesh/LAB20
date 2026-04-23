6fb853da99847bfdcf3b72060b6d6d519643eb46
ArathiVenkatesh <arathiv768@gmail.com>
2026-04-23 08:10:25 +0530

    Update Calculator.java

--- src/main/java/com/bnmit/Calculator.java
+++ src/main/java/com/bnmit/Calculator.java
@@ -1,20 +1,30 @@
-package com.bnmit; 
- 
-public class Calculator { 
- 
-    public int add(int a, int b) { 
-        return a + b; 
-    } 
- 
-    public int subtract(int a, int b) { 
-        return a - b; 
-    } 
- 
-    public int multiply(int a, int b) { 
-        return a * b; 
-    } 
- 
-    public int divide(int a, int b) { 
-        return a / b; 
-    } 
+package com.bnmit;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    // ✅ ADD THIS MAIN METHOD
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        System.out.println("Addition: " + calc.add(2, 4));
+        System.out.println("Subtraction: " + calc.subtract(5, 3));
+        System.out.println("Multiplication: " + calc.multiply(2, 3));
+        System.out.println("Division: " + calc.divide(4, 2));
+    }
 }
